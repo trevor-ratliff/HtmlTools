cd8af76eec81a1d369937fb30a23787d7691237d
Trevor Ratliff <trevor.w.ratliff@gmail.com>
2014-09-11 15:17:39 -0400

    it's working at the moment

--- ColorFinder/scripts/ColorFinder.js
+++ ColorFinder/scripts/ColorFinder.js
@@ -33,11 +33,16 @@ var gintMarkerOffset = -5;
 //		Saturation -- the color's saturation
 //		Level -- the color's level (value)
 //		Alpha -- the transparency of the color
+//		Id -- the textual id of the tile element
+//		Tile -- the div holding all of a color group's data
+//		Color -- the color element of a color group
+//		Display -- the information element off a color group
+//		Mark -- the adjustable element of a color group
 //
 //  Methods:
-//		GetColor() -- returns the color in a hsla() srting
-//		GetColorRGB() -- returns the color in a rgb() sting
-//		GetColorWeb() -- returns the color in a #RRGGBB srting
+//		GetColor() -- returns the color in a hsla() string
+//		GetColorRGB() -- returns the color in a rgb() string
+//		GetColorWeb() -- returns the color in a #RRGGBB string
 //
 //  Events:
 //
@@ -51,6 +56,7 @@ var gintMarkerOffset = -5;
 var ColorObject = function (vstrID) {
 	var self = this;
 	var lobjHueMark = document.getElementById('hueMark');
+	var lobjContrastOffset = document.getElementById('txtContrastOffset');
 	
 	//----
 	// set properties
@@ -69,8 +75,49 @@ var ColorObject = function (vstrID) {
 	//----
 	// get data
 	//----
+	if (this.Mark) this.Mark.style.left = this.Mark.style.left == "" ? gintMarkerOffset + 'px' : this.Mark.style.left;
 	
-	return this;
+	switch (this.Id) {
+		case 'contrast':		// opposite of hue on color wheel
+			this.Hue = (this.Hue + 180) % 360;
+			this.Level = '50%';
+			this.Saturation = '100%';
+			break;
+		
+		case 'contrastLeft':	// opposite of hue minus a set value ~15-30 degrees usually
+			this.Hue = ((this.Hue + 180 - parseInt(lobjContrastOffset.value)) % 360);
+			this.Level = '50%';
+			this.Saturation = '100%';
+			break;
+		
+		case 'contrastRight':	// opposite of hue plus a set value ~15-30 degrees usually
+			this.Hue = ((this.Hue + 180 + parseInt(lobjContrastOffset.value)) % 360);
+			this.Level = '50%';
+			this.Saturation = '100%';
+			break;
+		
+		case 'hue':				// Pure color
+			this.Level = '50%';
+			this.Saturation = '100%';
+			break;
+		
+		case 'shade':			// Pure color mixed with black (level < 50%)
+			this.Level = ((parseInt(this.Mark.style.left) % 50) - gintMarkerOffset) + '%';
+			this.Saturation = '100%';
+			break;
+		
+		case 'tint':			// Pure color mixed with white (level > 50%)
+			this.Level = ((parseInt(this.Mark.style.left) - gintMarkerOffset) % 50 + 50) + '%';
+			this.Saturation = '100%';
+			break;
+		
+		case 'tone':			// Pure color desaturated (saturation < 100%)
+			this.Level = '50%';
+			this.Saturation = (parseInt(this.Mark.style.left) - gintMarkerOffset) + '%';
+			break;
+	}
+	
+	return self;
 };
 
 
@@ -79,7 +126,7 @@ var ColorObject = function (vstrID) {
 /// @brief gets the color in a hsla() format
 /// @author Trevor Ratliff
 /// @date 2014-09-10
-/// @return string
+/// @return object -- {'string': 'hsla([hue], [saturation], [level], [alpha])', 'h': [hue], 's': [saturation], 'l': [level], 'a': [alpha]}
 //  
 //  Definitions:
 //  
@@ -90,9 +137,52 @@ var ColorObject = function (vstrID) {
 /// @endverbatim
 //====
 ColorObject.prototype.GetColor = function () {
-	return 'hsla(' + this.Hue + ', ' + this.Saturation +
-		', ' + this.Level + ', '+ this.Alpha + ')';
-}
+	return {
+		'string': 'hsla(' + this.Hue + ', ' + this.Saturation +
+			', ' + this.Level + ', '+ this.Alpha + ')',
+		'h': this.Hue,
+		's': this.Saturation,
+		'l': this.Level,
+		'a': this.Alpha 
+	};
+};
+
+
+//====
+/// @fn ColorObject.GetColorRGB()
+/// @brief gets the color in a rgba() format
+/// @author Trevor Ratliff
+/// @date 2014-09-11
+/// @return object -- {'string': '', 'r': [red], 'g': [green], 'b': [blue], 'a': [alpha]}
+/// 
+/// HOW TO RETURN hsl.to.rgb(h, s, l): 
+/// 	SELECT: 
+/// 		l<=0.5: PUT l*(s+1) IN m2
+/// 		ELSE: PUT l+s-l*s IN m2
+/// 	PUT l*2-m2 IN m1
+/// 	PUT hue.to.rgb(m1, m2, h+1/3) IN r
+/// 	PUT hue.to.rgb(m1, m2, h    ) IN g
+/// 	PUT hue.to.rgb(m1, m2, h-1/3) IN b
+/// 	RETURN (r, g, b)
+/// 
+/// HOW TO RETURN hue.to.rgb(m1, m2, h): 
+/// 	IF h<0: PUT h+1 IN h
+/// 	IF h>1: PUT h-1 IN h
+/// 	IF h*6<1: RETURN m1+(m2-m1)*h*6
+/// 	IF h*2<1: RETURN m2
+/// 	IF h*3<2: RETURN m1+(m2-m1)*(2/3-h)*6
+/// 	RETURN m1
+//  
+//  Definitions:
+//  
+/// @verbatim
+/// History:  Date  |  Programmer  |  Contact  |  Description  |
+///     2014-09-11  |  Trevor Ratliff  |  trevor.w.ratliff@gmail.com  |  
+///         function creation from info found at 'http://www.w3.org/TR/2011/REC-css3-color-20110607/#hsl-color'  |
+/// @endverbatim
+//====
+
+
 
 //====
 /// @fn MarkerAddMouseMove()
@@ -178,11 +268,19 @@ function MarkerRemoveMouseMove() {
 function MoveMarker(){
 	var event = (typeof(event) == "undefined") ? arguments[0] : event;
 	var lobjE = this.querySelector('.mark');
+	
 	var lintMin = parseInt(lobjE.getAttribute('markmin'));
 	var lintMax = parseInt(lobjE.getAttribute('markmax'));
 	var lintX = lobjE.style.left == '' ? gintMarkerOffset : parseInt(lobjE.style.left);
 	var lintOffsetX = event.clientX - gintMouseX;
 	var lintNewX = lintX + lintOffsetX;
+	var lobjContrast = new ColorObject('contrast');
+	var lobjContrastLeft = new ColorObject('contrastLeft');
+	var lobjContrastRight = new ColorObject('contrastRight');
+	var lobjHue = new ColorObject('hue');
+	var lobjShade = new ColorObject('shade');
+	var lobjTint = new ColorObject('tint');
+	var lobjTone = new ColorObject('tone');
 	
 	//----
 	// debug
@@ -208,6 +306,11 @@ function MoveMarker(){
 	gintMouseX = event.clientX;
 	gintMouseY = event.clientY;
 	
+	//----
+	// update screen colors
+	//----
+	UpdateScreen([lobjContrast, lobjContrastLeft, lobjContrastRight, lobjHue, lobjShade, lobjTint, lobjTone]);
+	
 	return;
 }
 
@@ -222,7 +325,7 @@ function MoveMarker(){
 //  
 /// @verbatim
 /// History:  Date  |  Programmer  |  Contact  |  Description  |
-///     _  |  Trevor Ratliff  |  trevor.w.ratliff@gmail.com  |  
+///     2014-09-10  |  Trevor Ratliff  |  trevor.w.ratliff@gmail.com  |  
 ///         function creation  |
 /// @endverbatim
 //====
@@ -231,6 +334,46 @@ function SetColors() {
 	// get references to tiles
 	//----
 	var lobjHueTile = document.getElementByID('hue');
+}
+
+
+//====
+/// @fn UpdateScreen(vobjColors)
+/// @brief updates the screen display of color groups passed in
+/// @author Trevor Ratliff
+/// @date 2014-09-11
+/// @param vobjColors -- ColorObject array
+//  
+//  Definitions:
+//  
+/// @verbatim
+/// History:  Date  |  Programmer  |  Contact  |  Description  |
+///     2014-09-11  |  Trevor Ratliff  |  trevor.w.ratliff@gmail.com  |  
+///         function creation  |
+/// @endverbatim
+//====
+function UpdateScreen(vobjColors) {
+	//----
+	// check for array
+	//----
+	if (vobjColors.length) {
+		//----
+		// loop through colors
+		//----
+		for (var lintII = 0; lintII < vobjColors.length; lintII++) {
+			//----
+			// set the color
+			//----
+			vobjColors[lintII].Color.style.backgroundColor = vobjColors[lintII].GetColor().string;
+			
+			//----
+			// set description data
+			//----
+			vobjColors[lintII].Display.querySelector('.hsl').innerHTML = vobjColors[lintII].GetColor().string;
+		}
+	}
+	
+	return;
 }
 
 
